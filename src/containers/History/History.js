451bb83b48d197865f0599475e45037591107c38
chenjianrui <sky75217@gmail.com>
2019-08-24 14:46:57 +0800

    test

--- src/containers/History/History.js
+++ src/containers/History/History.js
@@ -4,6 +4,7 @@ class History extends Component {
   componentDidMount() {
     const address = this.props.match.params.address;
     console.log(address);
+    console.log(address);
   }
   render() {
     return <div>History</div>;
